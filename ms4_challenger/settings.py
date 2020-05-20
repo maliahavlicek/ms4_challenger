f61488249941998cf55ef47c8994b74efa8369d9
maliahavlicek <malia.havlicek@gmail.com>
2020-05-19 20:08:32 -0600

    flip method for testing prod/test vs local dev with else clause

--- ms4_challenger/settings.py
+++ ms4_challenger/settings.py
@@ -19,6 +19,8 @@ if os.path.exists('env.py'):
     import env
 
     DEBUG = True
+else:
+    DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
