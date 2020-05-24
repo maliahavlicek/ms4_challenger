dbe8c1e58bf0367c7ca9fc84b56014606af18127
maliahavlicek <malia.havlicek@gmail.com>
2020-05-24 11:28:10 -0600

    Fine tune products display CSS

---
+++ static/js/products.js
@@ -0,0 +1,41 @@
+/* JS for products page */
+
+//on ready function
+$(document).ready(function () {
+    // initial alignment
+    alignProducts();
+    // add listener if user changes size of window/viewport
+    window.addEventListener('resize', function () {
+        alignProducts();
+    });
+});
+
+function alignProducts() {
+// figure out width of screen
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+    // set array of selectors we want to match heights for:
+    var selectors = ['.product-img-wrap', '.product-description', '.product-features', '.card.text.limits'];
+    $.each(selectors, function (index, selector) {
+        // First auto height things
+        var maxHeight = 'auto';
+        $(selector).height(maxHeight);
+        // next if larger than small devices, find max-height
+        if (vw > 767) {
+            var maxHeight = 0;
+
+            // loop through list of selectors to get maxHeight
+            $(selector).each(function () {
+                if ($(this).height() > maxHeight) {
+                    maxHeight = $(this).height();
+                }
+            });
+
+            // set all of that selector to the same height
+            $(selector).height(maxHeight);
+
+        }
+    });
+}
+
+
