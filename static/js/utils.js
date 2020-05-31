c3aa47b3f86dcfd3c6d1dd417015bdd67e074da3
maliahavlicek <malia.havlicek@gmail.com>
2020-05-30 18:03:25 -0600

    skipSmall was not set, switched timeout call in utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -3,29 +3,32 @@
 
 /* Match heights of items on page based on incoming array of selectors */
 function alignItems(selectors, skipSmall) {
-    //wait a little bit before moving forward
-    setTimeout(function () {  var skip_small = (typeof skipSmall === 'undefined') ? true : skip_small;}, 300);
+    var skip_small = (typeof skipSmall === 'undefined') ? true : skip_small;
 
 // figure out width of screen
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    $.each(selectors, function (index, selector) {
-        // First auto height things
-        var maxHeight = 'auto';
-        $(selector).height(maxHeight);
-        // next if larger than small devices, or if skipSmall is false find max-height and even things up
-        if (vw > 767 || !skip_small) {
-            var maxHeight = 0;
 
-            // loop through list of selectors to get maxHeight
-            $(selector).each(function () {
-                if ($(this).height() > maxHeight) {
-                    maxHeight = $(this).height();
-                }
-            });
-
-            // set all of that selector to the same height
+    //wait a little bit before moving forward
+    setTimeout(function () {
+        $.each(selectors, function (index, selector) {
+            // First auto height things
+            var maxHeight = 'auto';
             $(selector).height(maxHeight);
+            // next if larger than small devices, or if skipSmall is false find max-height and even things up
+            if (vw > 767 || !skip_small) {
+                var maxHeight = 0;
 
-        }
-    });
+                // loop through list of selectors to get maxHeight
+                $(selector).each(function () {
+                    if ($(this).height() > maxHeight) {
+                        maxHeight = $(this).height();
+                    }
+                });
+
+                // set all of that selector to the same height
+                $(selector).height(maxHeight);
+
+            }
+        });
+    }, 300);
 }
